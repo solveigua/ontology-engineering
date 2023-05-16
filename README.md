6fbc9edb73b85cb9daf8e17f4286f28be224d09b
psycalmatic <makhupane@gmail.com>
2023-05-16 13:36:01 +0200

    Base Webapp structure
    SpringBoot 3.0.6, Java17 for Verbalizer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ontology-engineering
 Class project for class CSC5028Z: Ontology Engineering @ UCT
+
+1.Ensure you have maven install
+2. Ensure you run (Only the first developer needs to do this): mvn -Nio.takari:maven:wrapper
---
+++ src/main/java/com/ontology/verbalizer/VerbalizerApplication.java
@@ -0,0 +1,13 @@
+package com.ontology.verbalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbalizerApplication.class, args);
+	}
+
+}
