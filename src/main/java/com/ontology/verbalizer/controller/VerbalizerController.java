981d843c04e908f1d7e5f9e9cabebc480fbe4c7a
Phuthang Makhupane <makhupane@gmail.com>
2023-05-18 00:54:32 +0200

    Update VerbalizerController.java
    Adding dependency injection - Service layer

--- src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
+++ src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
@@ -1,8 +1,13 @@
 package com.ontology.verbalizer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.ontology.verbalizer.service.NorwegianVerbalizerService;
+import com.ontology.verbalizer.service.SesothoVerbalizerService;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -10,6 +15,11 @@ import java.io.IOException;
 
 @RestController
 public class VerbalizerController {
+
+    @Autowired
+    NorwegianVerbalizerService norwegianVerbalizationService;
+    @Autowired
+    SesothoVerbalizerService sesothoVerbalizationService;
 
     @GetMapping("/")
     public void landingPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -20,13 +30,19 @@ public class VerbalizerController {
     @ResponseBody
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
-            try {
-                String content = new String(file.getBytes());
-                // Process the file content or perform any necessary operations
-                return "File uploaded successfully. Content: " + content;
-            } catch (IOException e) {
-                return "Error occurred while processing the file.";
+            String language = ""; // TODO: obtain language from request
+            String verbalization = "";
+
+            // If language choice is Norwegian, redirect to Norwegian engine
+            // If language choice is Sesotho, redirect to Sesotho engine
+            if (language.equals("Norwegian")) {
+                verbalization = norwegianVerbalizationService.getNorwegianVerbalization(file);
+            } else if (language.equals("SeSotho")) {
+                verbalization = sesothoVerbalizationService.getSesothoVerbalization(file);
+            } else {
+                return "Invalid language.";
             }
+            return verbalization;
         } else {
             return "Please select a file to upload.";
         }
