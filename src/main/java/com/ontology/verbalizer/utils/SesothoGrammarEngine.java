7513b7f0ae023efc4d69b8444380ed0a3c709e8d
Phuthang Makhupane <makhupane@gmail.com>
2023-05-21 13:42:41 +0200

    Adding Sesotho Grammar Engine
    Initial Commit of Sesotho grammar engine. This will evolve a lot over time. Committing to save progress.

---
+++ src/main/java/com/ontology/verbalizer/utils/SesothoGrammarEngine.java
@@ -0,0 +1,7 @@
+package com.ontology.verbalizer.utils;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public interface SesothoGrammarEngine {
+    public String getSesothoVerbalization(OWLOntology ontology);
+}
