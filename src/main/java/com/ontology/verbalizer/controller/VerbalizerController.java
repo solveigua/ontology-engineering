0fe818a7fc1ee780ebe0351e1eee9b2a7a29ab94
solveigua <solveig.aune@live.no>
2023-05-23 17:52:01 +0200

    WIP translation-page

--- src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
+++ src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.ontology.verbalizer.service.VerbalizerService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
-public class VerbalizerController {
+public class VerbalizerController extends HttpServlet {
 
     @Autowired
     VerbalizerService verbalizationService;
@@ -19,18 +22,16 @@ public class VerbalizerController {
         response.sendRedirect(request.getContextPath() + "/index.html");
     }
 
-    @GetMapping("/upload")
     @ResponseBody
-    @PostMapping("/upload")
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @RequestMapping(value = "/upload", method = { RequestMethod.POST, RequestMethod.GET })
     public String uploadFile(@RequestPart("inputFile") MultipartFile inputFile,
-            @RequestParam("language") String language) {
+            @RequestParam("language") String language, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
         String content = "";
         System.out.println("File name: " + inputFile.getName());
         if (!inputFile.isEmpty()) {
             try {
                 content += new String(inputFile.getBytes());
-                // return "File uploaded successfully. Content: " + content;
             } catch (IOException e) {
                 return "Error occurred while processing the file.";
             }
@@ -38,10 +39,20 @@ public class VerbalizerController {
 
         if (content != null && !content.isEmpty()) {
             String verbalization = verbalizationService.getVerbalization(content, language);
+            request.setAttribute("msg", verbalization);
+            System.out.println(request.getAttribute("msg"));
+            request.getRequestDispatcher("/translation.html").include(request, response);
+            translationPage(request, response);
             return verbalization;
         } else {
             return "Please provide the file content.";
         }
 
+    }
+
+    @RequestMapping(value = "/translation", method = RequestMethod.GET)
+    public String translationPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect(request.getContextPath() + "/translation.html");
+        return "translation";
     }
 }
