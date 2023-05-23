c85538380d58c59e20fd2ea43850e728b326f5e3
hageningrid <ingrid-hagen99@hotmail.com>
2023-05-23 15:52:36 +0200

    norwegianDisjoint now working

--- src/main/java/com/ontology/verbalizer/utils/norwegian/NorwegianSentenceVerbalizerImpl.java
+++ src/main/java/com/ontology/verbalizer/utils/norwegian/NorwegianSentenceVerbalizerImpl.java
@@ -1,6 +1,7 @@
 package com.ontology.verbalizer.utils.norwegian;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -28,8 +29,13 @@ public class NorwegianSentenceVerbalizerImpl implements NorwegianSentenceVerbali
 
     @Override
     public String verbalizeNorwegianDisjointClassesAxiom(List<String> classExpressions) {
-        String sentence = String.join(" er ikke det samme som en/et  ", classExpressions);
-        return sentence;
+        
+        String sentence = classExpressions.stream()
+            .map(n -> String.valueOf(n))
+            .limit(classExpressions.size()-1)
+            .collect(Collectors.joining(", "));
+
+        return sentence + " og " + classExpressions.get(classExpressions.size()-1) + " er ikke det samme.";
     }
 
     @Override
