a5ec4b07cef295ada84dd03d22686cd2e387a87e
solveigua <solveig.aune@live.no>
2023-05-24 09:07:49 +0200

    Rebase and redo

--- src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
+++ src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
@@ -4,46 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.ontology.verbalizer.service.VerbalizerService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import com.ontology.verbalizer.utils.Response;
 
 @RestController
+@RequestMapping("/verbalizer")
 public class VerbalizerController {
 
     @Autowired
     VerbalizerService verbalizationService;
 
-    @GetMapping("/")
-    public void landingPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect(request.getContextPath() + "/index.html");
-    }
-
-    @GetMapping("/upload")
-    @ResponseBody
     @PostMapping("/upload")
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String uploadFile(@RequestPart("inputFile") MultipartFile inputFile,
-            @RequestParam("language") String language, HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    public Response uploadFile(@RequestPart("inputFile") MultipartFile inputFile,
+            @RequestParam("language") String language) throws IOException {
         String content = "";
-        System.out.println("File name: " + inputFile.getName());
+
         if (!inputFile.isEmpty()) {
             try {
                 content += new String(inputFile.getBytes());
-                // return "File uploaded successfully. Content: " + content;
             } catch (IOException e) {
-                return "Error occurred while processing the file.";
+                System.out.println(e);
             }
         }
-        response.sendRedirect(request.getContextPath() + "/upload.html");
 
         if (content != null && !content.isEmpty()) {
             String verbalization = verbalizationService.getVerbalization(content, language);
-            return verbalization;
+            return new Response(verbalization);
         } else {
-            return "Please provide the file content.";
+            return new Response("Could not create verbalization");
         }
-
     }
 }
---
+++ src/main/java/com/ontology/verbalizer/utils/Response.java
@@ -0,0 +1,9 @@
+package com.ontology.verbalizer.utils;
+
+public class Response {
+    public String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+}
