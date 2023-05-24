6b275d7e60ef675ccdbd86dbe94664501bf8e05e
solveigua <solveig.aune@live.no>
2023-05-24 08:48:43 +0200

    Make translation show on index page

--- src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
+++ src/main/java/com/ontology/verbalizer/controller/VerbalizerController.java
@@ -11,58 +11,30 @@ import org.springframework.ui.Model;
 import com.ontology.verbalizer.utils.Response;
 
 @RestController
+@RequestMapping("/verbalizer")
 public class VerbalizerController {
 
     @Autowired
     VerbalizerService verbalizationService;
 
-    @GetMapping("/")
-    public void landingPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendRedirect(request.getContextPath() + "/index.html");
-    }
-
-    @GetMapping("/upload")
-    @ResponseBody
     @PostMapping("/upload")
-    @RequestMapping(value = "/upload", method = { RequestMethod.POST, RequestMethod.GET })
     public Response uploadFile(@RequestPart("inputFile") MultipartFile inputFile,
-            @RequestParam("language") String language, HttpServletRequest request, HttpServletResponse response,
-            Model m, @ModelAttribute("message") String message)
-            throws IOException {
+            @RequestParam("language") String language) throws IOException {
         String content = "";
 
-        // System.out.println("File name: " + inputFile.getName());
         if (!inputFile.isEmpty()) {
             try {
                 content += new String(inputFile.getBytes());
-                // return "File uploaded successfully. Content: " + content;
             } catch (IOException e) {
-                // return "Error occurred while processing the file.";
                 System.out.println(e);
             }
         }
 
         if (content != null && !content.isEmpty()) {
-            String verbalization = verbalizationService.getVerbalization(content,
-                    language);
-            m.addAttribute("message", verbalization);
-            // System.out.println(m.getAttribute("message"));
-            // loadTranslationPage(m, response, request, verbalization, "");
-            Response r = new Response(verbalization);
-            response.sendRedirect(request.getContextPath() + "/translation.html");
-            System.out.println(r);
-            return r;
+            String verbalization = verbalizationService.getVerbalization(content, language);
+            return new Response(verbalization);
         } else {
             return new Response("Could not create verbalization");
         }
     }
-
-    @GetMapping("/translaion")
-    public void loadTranslationPage(HttpServletResponse response,
-            HttpServletRequest request)
-            throws IOException {
-        response.sendRedirect(request.getContextPath() + "/translation.html");
-        System.out.println("HELLO FROM LOADTRANSLATION");
-    }
-
 }
